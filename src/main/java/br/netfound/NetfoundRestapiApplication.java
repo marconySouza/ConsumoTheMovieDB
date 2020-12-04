e81edf615d3c5d11a8eb94c6fc9c79d066490eb5
marconySouza <54107073+marconySouza@users.noreply.github.com>
2020-12-04 02:07:54 -0300

    Update NetfoundRestapiApplication.java

--- src/main/java/br/netfound/NetfoundRestapiApplication.java
+++ src/main/java/br/netfound/NetfoundRestapiApplication.java
@@ -2,7 +2,13 @@ package br.netfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * @author marcony.souza
@@ -15,5 +21,17 @@ public class NetfoundRestapiApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(NetfoundRestapiApplication.class, args);
 	}
+	
+	@Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 
 }
